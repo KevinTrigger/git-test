631b36c30fb4c55c92e4ac85f7809d2d8e5b9e6f
Sergey <frankgo14@mail.ru>
2023-07-16 19:54:17 +0500

    changed objects App.js

--- src/App.js
+++ src/App.js
@@ -1,32 +1,30 @@
-import { useState } from 'react';
-import Card from './components/Card.jsx';
+import { useState } from "react";
+import Card from "./components/Card.jsx";
 
 function App() {
-  const [rapAlbums, setRapAlbums] = useState(
-    [
+  const [rapAlbums, setRapAlbums] = useState([
     {
-      title: 'The Slim Shady LP',
-      author: 'Eminem',
-      year: 1999, 
+      title: "The Slim Shady LP",
+      author: "Eminem",
+      year: 1999,
       cover: "slim-shady.jpg",
     },
     {
-      title: 'Кара-Тэ',
+      title: "TAPOK",
       year: 2004,
       cover: "kara-te.jpg",
     },
-  ]
-  )
+  ]);
 
   return (
     <div className="App bg-orange-400">
-      <div className='max-w-1200 mx-auto p-5 '>
+      <div className="max-w-1200 mx-auto p-5 ">
         {rapAlbums.map((card, index) => (
-          <Card card={card} key={index}/>
+          <Card card={card} key={index} />
         ))}
       </div>
     </div>
-  )
+  );
 }
 
 export default App;
